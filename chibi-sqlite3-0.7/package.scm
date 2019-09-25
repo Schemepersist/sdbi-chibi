1f233f01669e394cc405210c3b596da064fedd41
Harold Ancell <hga@ancell-ent.com>
2019-09-25 06:46:04 -0500

    import snow package chibi-sqlite3-0.7

---
+++ chibi-sqlite3-0.7/package.scm
@@ -0,0 +1,11 @@
+(package
+  (version "0.7")
+  (library
+    (name
+      (chibi sqlite3))
+    (path "chibi/sqlite3.sld")
+    (depends
+      (chibi)
+      (scheme base)
+      (scheme write)
+      (srfi 130))))
